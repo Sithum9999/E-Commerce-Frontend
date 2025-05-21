2a38c140aad8d4f27ea6a5380a2c9004abb74d6a
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-21 16:25:36 +0530

    update coupons component html and ts files

--- src/app/admin/component/coupons/coupons.component.ts
+++ src/app/admin/component/coupons/coupons.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../service/admin.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-coupons',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTableModule, MatCardModule],
   templateUrl: './coupons.component.html',
   styleUrl: './coupons.component.css'
 })
 export class CouponsComponent {
+
+  coupons: any;
+
+  constructor(private adminService: AdminService) { } 
+
+  ngOnInit(): void {
+    this.getCoupons();
+  }
+
+  getCoupons(): void {
+    this.adminService.getCoupons().subscribe((res: any) => {
+      this.coupons = res;
+    });
+  }
 
 }
