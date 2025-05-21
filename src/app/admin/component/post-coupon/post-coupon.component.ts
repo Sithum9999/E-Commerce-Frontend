a5070dbe0afdda83f401edfc84ce7c11ac61be8d
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-21 16:08:10 +0530

    update post - coupon html and ts files

--- src/app/admin/component/post-coupon/post-coupon.component.ts
+++ src/app/admin/component/post-coupon/post-coupon.component.ts
@@ -1,12 +1,60 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { AdminService } from '../../service/admin.service';
 
 @Component({
   selector: 'app-post-coupon',
   standalone: true,
-  imports: [],
+  imports: [NgIf, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatNativeDateModule],
   templateUrl: './post-coupon.component.html',
   styleUrl: './post-coupon.component.css'
 })
 export class PostCouponComponent {
+
+  couponForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private adminService: AdminService
+  ) { }
+
+  ngOnInit(): void {
+    this.couponForm = this.fb.group({
+      name: [null, [Validators.required]],
+      code: [null, [Validators.required]],
+      discount: [null, [Validators.required]],
+      expirationDate: [null, [Validators.required]],
+    });
+  }
+
+  addCoupon() {
+    if (this.couponForm.valid) {
+      this.adminService.addCoupon(this.couponForm.value).subscribe((res) => {
+        if (res.id != null) {
+          this.snackBar.open('Coupon Posted Successfully!', 'Close', {
+            duration: 5000
+          });
+          this.router.navigateByUrl('/admin/dashboard');
+        } else {
+          this.snackBar.open(res.message, 'Close', {
+            duration: 5000,
+            panelClass: 'error-snackbar'
+          });
+        }
+      });
+    } else {
+      this.couponForm.markAllAsTouched();
+    }
+  }
 
 }
