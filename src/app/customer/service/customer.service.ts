f5eeb2c3cf94e85e8d768fd5c3aa0e4ffb962be0
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-22 17:30:52 +0530

    create increaseProductQuantity() in customer.ts

--- src/app/customer/service/customer.service.ts
+++ src/app/customer/service/customer.service.ts
@@ -35,6 +35,17 @@ export class CustomerService {
     });
   }
 
+  increaseProductQuantity(productId: any): Observable<any> {
+  const cartDto = {
+    productId: productId,
+    userId: UserStorageService.getUserId()
+  };
+
+  return this.http.post(BASIC_URL + '/api/customer/addition', cartDto, {
+    headers: this.createAuthorizationHeader(),
+  });
+}
+
   getCartByUserId(): Observable<any> {
     const userId = UserStorageService.getUserId();
     return this.http.get(BASIC_URL + `api/customer/cart/${userId}`, {
