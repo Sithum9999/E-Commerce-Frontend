91f8e8fd2195aaa99cf2b6bb780b330b6f1182b2
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-22 14:09:42 +0530

    updated cart.componenent.ts file

--- src/app/customer/component/cart/cart.component.ts
+++ src/app/customer/component/cart/cart.component.ts
@@ -1,16 +1,17 @@
-import { Component} from '@angular/core';
-import { FormBuilder, FormsModule} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatDialog } from '@angular/material/dialog';
 import { CustomerService } from '../../service/customer.service';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule,MatIconModule, FormsModule, MatButtonModule, NgFor, NgIf],
+  imports: [CommonModule, MatIconModule, FormsModule, MatButtonModule, NgFor, NgIf, MatInputModule, ReactiveFormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
@@ -19,22 +20,41 @@ export class CartComponent {
   cartItems: any[] = [];
   order: any;
 
-  constructor(private customerService: CustomerService, 
-              private snackbar: MatSnackBar, 
-              private fb: FormBuilder, 
-              private dialog: MatDialog) {
+  couponForm: FormGroup;
+
+  constructor(private customerService: CustomerService,
+    private snackbar: MatSnackBar,
+    private fb: FormBuilder,
+    private dialog: MatDialog) {
 
   }
 
   ngOnInit(): void {
+    this.couponForm = this.fb.group({
+      code: [null, [Validators.required]]
+    });
+
     this.getCart();
+  }
+
+  applyCoupon() {
+    this.customerService.applyCoupon(this.couponForm.get('code')!.value).subscribe(res => {
+      this.snackbar.open("Coupon Applied Successfully", 'Close', {
+        duration: 5000
+      });
+      this.getCart();
+    }, error => {
+      this.snackbar.open(error.error, 'Close', {
+        duration: 5000
+      });
+    });
   }
 
   getCart() {
     this.cartItems = [];
     this.customerService.getCartByUserId().subscribe(res => {
       console.log(res);
-      
+
       this.order = res;
       res.cartItems.forEach(element => {
         element.processedImg = 'data:image/jpeg;base64,' + element.returnedImg;
