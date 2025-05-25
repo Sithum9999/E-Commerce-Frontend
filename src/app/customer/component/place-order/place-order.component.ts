1c347edc73730fdee6c7bef2bf6d1c018a4841cc
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-25 13:11:17 +0530

    update place order componenent.ts

--- src/app/customer/component/place-order/place-order.component.ts
+++ src/app/customer/component/place-order/place-order.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { CustomerService } from '../../service/customer.service';
 
 @Component({
   selector: 'app-place-order',
@@ -8,5 +13,38 @@ import { Component } from '@angular/core';
   styleUrl: './place-order.component.css'
 })
 export class PlaceOrderComponent {
+
+  orderForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private customerService: CustomerService,
+    private router: Router,
+    public dialog: MatDialog
+  ) { }
+
+  ngOnInit() {
+    this.orderForm = this.fb.group({
+      address: [null, [Validators.required]],
+      orderDescription: [null]
+    });
+  }
+
+  placeOrder() {
+    this.customerService.placeOrder(this.orderForm.value).subscribe(res => {
+      if (res.id != null) {
+        this.snackBar.open("Order placed successfully", "Close", { duration: 5000 })
+        this.router.navigateByUrl("/customer/my-orders");
+        this.closeForm();
+      } else {
+        this.snackBar.open("Something went wrong", "Close", { duration: 5000 })
+      }
+    });
+  }
+
+  closeForm(){
+    this.dialog.closeAll();
+  }
 
 }
