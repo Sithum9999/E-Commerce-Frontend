fef146679d5cecb08578dbb159d2b9edf8b95db2
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-26 00:40:15 +0530

    update order component.ts

--- src/app/admin/component/orders/orders.component.ts
+++ src/app/admin/component/orders/orders.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AdminService } from '../../service/admin.service';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-orders',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatTableModule, MatCardModule, MatMenuModule, MatButtonModule],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
 export class OrdersComponent {
+
+  orders: any;
+
+  constructor(private adminService: AdminService,
+    private snackBar: MatSnackBar) { }
+
+  ngOnInit() {
+    this.getPlacedOrders();
+  }
+
+  getPlacedOrders() {
+    this.adminService.getPlacedOrders().subscribe(res => {
+      this.orders = res;
+    });
+  }
 
 }
