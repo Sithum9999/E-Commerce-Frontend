6e6377550301b1723248995053596b1131ebdf5c
Sithum9999 <sankalpasithum51@gmail.com>
2025-05-30 22:49:52 +0530

    create order by component and analytics component updated css ts html files

--- src/app/admin/component/analytics/analytics.component.ts
+++ src/app/admin/component/analytics/analytics.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../service/admin.service';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { OrderByStatusComponent } from './order-by-status/order-by-status.component';
 
 @Component({
   selector: 'app-analytics',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule,],
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.css'
 })
 export class AnalyticsComponent {
+
+  data:any;
+
+  constructor(private adminService: AdminService) { }
+
+  ngOnInit() {
+    this.adminService.getAnalytics().subscribe(res => {
+      console.log(res);
+      this.data=res;
+    })
+  }
 
 }
